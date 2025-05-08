96aa7bbb0036b8141dd2963e7dcd36bfab49ac05
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-08 20:24:32 +0700

    update memperbarui materi yang belum selesai

--- 1. Memulai Pemrograman dengan Python/9.object-oriented programming (OOP).py
+++ 1. Memulai Pemrograman dengan Python/9.object-oriented programming (OOP).py
@@ -15,3 +15,29 @@ Class, object, dan method akan kita bahas secara mendalam pada materi kali ini. 
 Secara umum, konsep OOP dalam pemrograman sangat mirip seperti ilustrasi-ilustrasi di atas. Object-oriented programming adalah paradigma pemrograman berorientasi pada pengorganisasian kode menjadi objek-objek yang memiliki atribut dan perilaku (method). Objek merupakan perwujudan dari class dengan anggapan bahwa kelas adalah cetakan yang memungkinkan kita dapat membuat banyak objek berdasarkan cetakan tersebut.
 
 """
+
+# Class
+# Pembuatan class dalam Python mirip seperti fungsi, yakni perlu menggunakan keyword untuk bisa membuatnya. Keyword atau kata kunci untuk membuat kelas dalam Python adalah "class".
+class Mobil: 
+    warna = "merah" # atribut
+
+# Object (Objek)
+# Objek adalah perwujudan dari class. Kita bisa membuat objek dari class yang sudah kita buat sebelumnya. Untuk membuat objek, kita cukup memanggil nama class tersebut layaknya kita memanggil fungsi.
+
+mobil_1 = Mobil() # objek dari class Mobil
+mobil_1.warna = "biru" # mengubah atribut warna dari objek mobil_1
+print(mobil_1.warna) # output: merah ke biru, nama objek dan atributnya
+
+
+# Atribut
+# Dalam Python, ada dua jenis atribut kelas yang dapat dibagi, yaitu atribut kelas dan atribut objek atau instance. Atribut kelas adalah jenis atribut yang secara otomatis terdefinisi dan menjadi bawaan kelas ketika instance dibuat berdasarkan kelas tersebut. Anda dapat menganggapnya sebagai nilai default atau bawaan dari kelas. Jika Anda membuat beberapa objek berdasarkan kelas yang memiliki jenis atribut ini, setiap objek akan memiliki atribut yang sama dengan nilai yang sama. 
+
+mobil_1 = Mobil()
+print(mobil_1.warna) # output: merah
+mobil_2 = Mobil() 
+print(mobil_2.warna) # output: merah
+
+Mobil.warna = "hitam" # mengubah atribut warna dari class Mobil
+
+print(mobil_1.warna) # output: hitam
+print(mobil_2.warna) # output: hitam
