3b1649dca2476806308552dd5ea98b8544bdd9ec
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-13 16:23:00 +0700

    update

--- 1. Memulai Pemrograman dengan Python/12.library/library_text_processing.py
+++ 1. Memulai Pemrograman dengan Python/12.library/library_text_processing.py
@@ -1,1 +1,24 @@
-# rext pro
+# Library Text Processing
+# Pertama adalah sekumpulan library yang bertujuan untuk melakukan pemrosesan teks dan menyederhanakan serta mempercepat tugas-tugas pemrosesan teks.
+
+
+
+"""
+
+String
+String adalah salah satu modul bawaan Python yang tidak perlu dideklarasikan. Pada modul string ada fungsi-fungsi yang dapat dioperasikan pada variabel bertipe string seperti di bawah.
+    1. upper(): Ubah setiap huruf dalam string menjadi huruf kapital. 
+    2. lower(): Ubah setiap huruf dalam string menjadi huruf kecil.
+    3. split(): Pisahkan teks berdasarkan delimiter (karakter pemisah).
+    4. title(): Jadikan setiap awal kata kapital.
+    5. zfill(): Tambahkan nol di awal string sebanyak nilai yang ada pada parameter.
+
+"""
+
+testing = "Pelajar"
+print(testing.upper())
+print(testing.lower())
+print(testing.split())
+print(testing.title())
+print(testing.zfill())
+
