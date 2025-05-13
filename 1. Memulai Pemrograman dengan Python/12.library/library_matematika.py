7436c0824ff8d7e28bca1d1ebb2748e7b6e47e71
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-13 22:14:13 +0700

    update

---
+++ 1. Memulai Pemrograman dengan Python/12.library/library_matematika.py
@@ -0,0 +1,16 @@
+"""
+Library Matematika
+Selanjutnya adalah library math yang termasuk salah satu modul bawaan Python dan menyediakan berbagai fungsi dan konstanta matematika. Anda hanya perlu melakukan impor untuk modul math. Berikut contoh penerapannya. 
+
+"""
+
+import math
+
+print(math.sqrt(200)) # sqrt mengitung akar kuadrat
+print(math.pi)
+
+"""
+Pada contoh di atas, hal pertama yang dilakukan adalah melakukan impor modul math untuk menyediakan berbagai fungsi dan konstanta matematika. Di bawahnya, kita mencoba melakukan operasi akar dari bilangan 25 yang hasilnya adalah 5. Kemudian, kita mencoba mendapatkan nilai pi dalam modul math yang bernilai “3.141592653589793”.
+
+"""
+
--- 1. Memulai Pemrograman dengan Python/12.library/library_text_processing.py
+++ 1. Memulai Pemrograman dengan Python/12.library/library_text_processing.py
@@ -1,11 +1,11 @@
 # Library Text Processing
 # Pertama adalah sekumpulan library yang bertujuan untuk melakukan pemrosesan teks dan menyederhanakan serta mempercepat tugas-tugas pemrosesan teks.
 
-
+import re # import modul regex
 
 """
 
-String
+1. String
 String adalah salah satu modul bawaan Python yang tidak perlu dideklarasikan. Pada modul string ada fungsi-fungsi yang dapat dioperasikan pada variabel bertipe string seperti di bawah.
     1. upper(): Ubah setiap huruf dalam string menjadi huruf kapital. 
     2. lower(): Ubah setiap huruf dalam string menjadi huruf kecil.
@@ -20,5 +20,25 @@ print(testing.upper())
 print(testing.lower())
 print(testing.split())
 print(testing.title())
-print(testing.zfill())
+print(testing.zfill(20))
 
+
+"""
+
+2. Regex
+Regex atau regular expression adalah sebuah cara untuk mencari teks berdasarkan pola tertentu. Umpamanya, ketika ingin mencari sebuah kata dalam kamus, misalnya arti dari kata parsing, kita akan mencari kata tersebut di halaman yang memiliki kata dengan awalan p, lalu pa. Regex bekerja dengan konsep yang sama.
+
+Pada regex, kita mencari sebuah kata atau kumpulan kata dengan memberikan pola yang diinginkan. Contoh umum regex adalah pada email. Kita dapat menggunakan regex untuk mengecek bahwa karakter @ ada pada email atau tidak.
+
+Contoh di bawah menunjukkan penggunaan regex. Pada variabel pattern di bawah, ^a berarti kita ingin mencari teks dengan awalan 'a', dan s$ berarti kita ingin mencari string berakhiran 's'.
+
+"""
+
+pola = '^a....s$' 
+string_test = 'acycss'
+hasil = re.match(pola, string_test)
+
+if hasil:
+    print('Berhasil')
+else:
+    print('Pencarian Gagal')
