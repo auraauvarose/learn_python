1160805a3e389aab48987706c1f541c2cdfbe1c6
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-14 12:32:13 +0700

    update

---
+++ 1. Memulai Pemrograman dengan Python/12.library/library_pengolahan-data/library_pandas.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+"""
+Pandas adalah library populer yang digunakan untuk pengelolaan dan analisis data. Library ini menyediakan struktur data dan alat untuk membantu pengguna dalam melakukan manipulasi, pembersihan, transformasi, dan analisis data dengan mudah dan efisien.
+
+Meskipun pandas sudah terinstal secara otomatis pada beberapa IDE dan versi Python, perlu diingat bahwa pandas bukan merupakan library bawaan Python. Oleh karena itu, Anda harus menginstal library ini terlebih dahulu sebelum dapat menggunakannya. Silakan buka command prompt dan jalankan kode berikut.
+
+"""
+
+data = 
---
+++ 1. Memulai Pemrograman dengan Python/12.library/library_pengolahan-data/library_pengolahan_data.py
@@ -0,0 +1,7 @@
+"""
+Library Pengolahan Data
+Library pengolahan data bertujuan untuk membantu dalam manipulasi, analisis, dan pemrosesan data. Library ini menyediakan berbagai fungsi dan metode yang memudahkan pengguna untuk melakukan operasi pengolahan data dengan lebih efisien dan cepat.
+
+Tujuan dari library ini untuk menyederhanakan tugas-tugas kompleks yang berkaitan dengan pengolahan data sehingga Anda tidak perlu mengimplementasikan semuanya dari awal. Berikut adalah beberapa library populer yang digunakan untuk pengolahan data.
+
+"""
