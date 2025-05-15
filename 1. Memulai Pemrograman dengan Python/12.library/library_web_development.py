c7c1c017ee4402a3fd41349fe2ec3ba3de0a7c4c
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-16 01:53:31 +0700

    update file

---
+++ 1. Memulai Pemrograman dengan Python/12.library/library_web_development.py
@@ -0,0 +1,19 @@
+"""
+Terakhir, ada library yang bertujuan untuk pengembangan aplikasi web. Sebagaimana yang sudah dijelaskan dalam materi-materi sebelumnya, Python dapat digunakan untuk pengembangan aplikasi web pada sisi server. Berikut adalah library yang dapat digunakan untuk membantu Anda mengembangkan web.
+
+Django
+Django adalah high-level Python web framework yang mendukung pengembangan secara cepat, bersih, serta pragmatis.
+
+https://docs.djangoproject.com/en/4.2/topics/install/
+
+Flask
+Flask adalah web framework untuk Python yang ditujukan untuk membangun aplikasi web. Flask dirancang dengan tujuan menjadi ringan, fleksibel, dan sederhana.
+
+https://flask.palletsprojects.com/en/2.3.x/installation/
+
+Fast API
+FastAPI adalah web framework untuk Python yang tujuannya merancang dan membangun API dengan cepat, efisien, dan aman. FastAPI memberikan kinerja yang tinggi, sintaks yang intuitif, serta dukungan otomatisasi dokumentasi yang kuat. Jadi, ia cocok untuk pengembangan mikroservis, layanan web responsif, dan sebagainya.
+
+https://fastapi.tiangolo.com/
+
+"""
