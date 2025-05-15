8ef7cfce4add33225e536c3d2b937235ab01ed36
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-16 01:47:49 +0700

    update

---
+++ 1. Memulai Pemrograman dengan Python/12.library/library_machine_learning.py
@@ -0,0 +1,41 @@
+"""
+Library Machine Learning
+Selanjutnya adalah library yang digunakan untuk melakukan pemelajaran mesin. Anda dapat menggunakan library berikut untuk membantu Anda menyelesaikan permasalahan machine learning. Berikut adalah beberapa library populer untuk machine learning.
+
+"""
+
+
+# scikit-learn
+
+# Pertama adalah scikit-learn yang menyediakan berbagai algoritma pemelajaran mesin siap pakai untuk membantu dalam pengembangan model pemelajaran mesin, pemrosesan data, dan evaluasi kinerja model.
+
+"""
+Scikit-learn termasuk library eksternal sehingga Anda perlu menginstalnya terlebih dahulu untuk bisa menggunakannya. Silakan jalankan kode berikut untuk menginstalnya menggunakan pip.
+
+# pip install -U scikit-learn
+
+Jika Anda ingin menggunakan conda, silakan gunakan kode berikut.
+
+# conda create -n sklearn-env -c conda-forge scikit-learn 
+# conda activate sklearn-env
+
+Anda bisa merujuk pada tautan berikut untuk melihat berbagai penerapan machine learning menggunakan scikit-learn.
+
+https://scikit-learn.org/stable/auto_examples/release_highlights/plot_release_highlights_1_6_0.html#sphx-glr-auto-examples-release-highlights-plot-release-highlights-1-6-0-py
+"""
+
+
+# TensorFlow
+# Selanjutnya adalah TensorFlow sebagai salah satu library paling populer terkait machine learning. Dengan menggunakan TensorFlow, Anda bisa mengembangkan machine learning hingga tahap deployment.
+
+"""
+Anda bisa menginstal TensorFlow menggunakan perintah pip berikut.
+
+# pip install tensorflow
+
+Berikut adalah link dokumentasi yang bisa dijadikan rujukan untuk mempelajari machine learning menggunakan tensorflow.
+
+https://www.tensorflow.org/tutorials?hl=id
+"""
+
+
