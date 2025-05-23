38d24e370bfa963ef869b111004c42ab135028d9
? <110692056+auraauvarose@users.noreply.github.com>
2025-05-23 21:20:43 +0700

    update

--- 3. Belajar Machine Learning untuk Pemula/2.machine_learning workflow/6.latihan_data_loading.py
+++ 3. Belajar Machine Learning untuk Pemula/2.machine_learning workflow/6.latihan_data_loading.py
@@ -1,3 +1,10 @@
 import pandas as pd
 test = pd.read_csv("3. Belajar Machine Learning untuk Pemula/2.machine_learning workflow/test.csv")
-test.head()
+print(test.head())
+
+
+"""
+Catatan
+Walaupun terlihat sangat sederhana dan mudah loading dataset adalah langkah pertama dan esensial dalam proses machine learning. Proses ini melibatkan pengambilan data dari sumber eksternal dan memuatnya ke dalam lingkungan kerja untuk dianalisis lebih lanjut. Dengan dataset yang sudah dimuat, Anda bisa memulai proses eksplorasi, pembersihan, dan pelatihan model machine learning, yang semuanya bergantung pada kualitas dan kesiapan data yang Anda miliki.
+
+"""
